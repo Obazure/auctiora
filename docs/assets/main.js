af5ba33b2fe816ed9c747068f6fc7f63ee65f4db
Yeldar Kossynbay <yeldar.kossynbay@cobrainer.com>
2022-09-06 15:50:15 +0200

    Add back to top button.

---
+++ docs/assets/main.js
@@ -0,0 +1,23 @@
+function scrollToTop() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+}
+
+let scrollToTopButtons = undefined
+function listenScroll() {
+    if (scrollToTopButtons !== undefined && (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)) {
+        for (let i = 0; i < scrollToTopButtons.length; i++) {
+            scrollToTopButtons[i].style.display = "block";
+        }
+    } else {
+        for (let i = 0; i < scrollToTopButtons.length; i++) {
+            scrollToTopButtons[i].style.display = "none";
+        }
+    }
+}
+
+scrollToTopButtons = document.getElementsByClassName("btn-to-top");
+document.addEventListener('scroll', listenScroll)
