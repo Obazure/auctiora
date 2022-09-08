7ae4e8d681f575690756b072cfa23f8accf17065
Yeldar Kossynbay <yeldar.kossynbay@cobrainer.com>
2022-09-08 10:37:33 +0200

    Update scroll script.

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -3,10 +3,10 @@ function scrollToTop() {
         top: 0,
         left: 0,
         behavior: 'smooth'
-    })
+    });
 }
 
-let scrollToTopButtons = undefined
+let scrollToTopButtons = undefined;
 function listenScroll() {
     if (scrollToTopButtons !== undefined && (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)) {
         for (let i = 0; i < scrollToTopButtons.length; i++) {
@@ -19,5 +19,7 @@ function listenScroll() {
     }
 }
 
-scrollToTopButtons = document.getElementsByClassName("btn-to-top");
-document.addEventListener('scroll', listenScroll)
+document.addEventListener("DOMContentLoaded", () => {
+    scrollToTopButtons = document.getElementsByClassName("btn-to-top");
+    document.addEventListener('scroll', listenScroll);
+});
