0fb347a8a5dcb3909044d3000df8134c843d51ef
Yeldar Kossynbay <yeldar.kossynbay@cobrainer.com>
2022-09-08 11:22:35 +0200

    Fix scroll to top.

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -1,25 +1,23 @@
 function scrollToTop() {
-    window.scroll({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
 }
 
 let scrollToTopButtons = undefined;
 const listenScroll = () => {
-    if (scrollToTopButtons !== undefined && (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)) {
-        for (let i = 0; i < scrollToTopButtons.length; i++) {
-            scrollToTopButtons[i].style.display = "block";
-        }
-    } else {
-        for (let i = 0; i < scrollToTopButtons.length; i++) {
-            scrollToTopButtons[i].style.display = "none";
-        }
+  if (scrollToTopButtons !== undefined && (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)) {
+    for (let i = 0; i < scrollToTopButtons.length; i++) {
+      scrollToTopButtons[i].style.display = "block";
     }
-}
+  } else {
+    for (let i = 0; i < scrollToTopButtons.length; i++) {
+      scrollToTopButtons[i].style.display = "none";
+    }
+  }
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-    scrollToTopButtons = document.getElementsByClassName("btn-to-top");
-    document.addEventListener('scroll', listenScroll);
-});
+scrollToTopButtons = document.getElementsByClassName("btn-to-top");
+document.addEventListener("scroll", listenScroll);
